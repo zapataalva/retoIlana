5790eb94e5eddaedf664db40b4a794294cc46334
zapataalva <zapataalva4@gmail.com>
2024-01-30 00:52:40 -0500

    Se agrega el caso de numero feliz

--- src/main/java/utils/HappyNumber.java
+++ src/main/java/utils/HappyNumber.java
@@ -1,4 +1,31 @@
 package utils;
 
 public class HappyNumber {
+
+    public static boolean esNumeroFeliz(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = calcularSumaCuadrados(slow);
+            fast = calcularSumaCuadrados(calcularSumaCuadrados(fast));
+
+            if (fast == 1) {
+                return true;
+            }
+
+        } while (slow != fast);
+
+        return false;
+    }
+
+    private static int calcularSumaCuadrados(int num) {
+        int suma = 0;
+        while (num > 0) {
+            int digito = num % 10;
+            suma += digito * digito;
+            num /= 10;
+        }
+        return suma;
+    }
 }
